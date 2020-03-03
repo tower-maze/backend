13ce0cec313f138b6f529fd8455b63b9b33ca03b
William Sedlacek <wsedlacekc@gmail.com>
2020-03-03 11:17:21 -0600

    Disable Basic Auth Popup
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -123,7 +123,7 @@ REST_FRAMEWORK = {
     #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     # ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
