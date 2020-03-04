d6e7fec795715a47b1fae68c71d2ac2773cf4a5b
ericbanker12 <ericbanker12@gmail.com>
2020-03-04 10:05:02 -0600

    WIP rewrite model methods and remove Room model from database

--- adventure/models.py
+++ adventure/models.py
@@ -10,9 +10,13 @@ import random
 
 class Maze(models.Model):
     title = models.CharField(max_length=127, default="Default Title")
+    start_room = models.BinaryField(null=True)
+    exit_room = models.BinaryField(null=True)
+    seed = models.BinaryField(null=True)
+    rooms = None
 
     def initialize(self):
-        """returns 2D array grid of maze rooms, creating them if missing"""
+        """loads the maze rooms into memory, generating new rooms if seed is missing"""
         # [
         #     [Room(x=0,y=0), Room(x=1,y=0), ...],
         #     [Room(x=0,y=1), Room(x=1,y=1), ...],
@@ -20,63 +24,91 @@ class Maze(models.Model):
         # ]
         if not self.id:
             self.save()
-        rooms = Room.objects.filter(maze=self.id)
-        if len(rooms) != 32*32:
+        if not self.rooms:
+            # generate room objects O(n^2) n=32
+            self.rooms = [[None for i in range(32)] for j in range(32)]
             for y in range(32):
                 for x in range(32):
-                    room = Room.objects.create()
-                    room.x = x
-                    room.y = y
-                    room.maze = self.id
-                    room.save()
-            rooms = Room.objects.filter(maze=self.id)
-        return [[rooms[i] for i in range(j*32, (j+1)*32)] for j in range(32)]
+                    room = Room(x, y, self)
+                    self.rooms[y][x] = room
+            if not self.exit_room or not self.start_room:
+                # select start and exit, save as (x,y) tuples
+                x, y = random.randint(0,31), 0
+                self.exit_room = bytes((x, y))
+                x, y = random.randint(0,31), 31
+                self.start_room = bytes((x, y))
+            # generate connections
+            x, y = self.start_room
+            maze_start = self.rooms[y][x]
+            maze_stack = Stack()
+            maze_stack.push(maze_start)
+            seed = self.seed
+            if not seed:
+                seed = bytearray(32*32)
+            i = 0
+            # repeat until stack is empty O(n^2) n=32
+            while len(maze_stack):
+                room = maze_stack.get_head()
+                # pick a random, available direction
+                available_rooms = room.get_available_rooms(self.rooms)
+                if len(available_rooms):
+                    if self.seed:
+                        index = self.seed[i]
+                    else:
+                        index = random.choice(range(len(available_rooms)))
+                        seed[i] = index
+                    next_room = available_rooms[index]
+                    # connect and add next room to stack
+                    room.connect(next_room)
+                    maze_stack.push(next_room)
+                    i += 1
+                # if dead end, go back through stack
+                else:
+                    maze_stack.pop()
+            # save seed, start, and exit
+            if not self.seed:
+                self.seed = seed
+            self.save()
 
-    def generate_connections(self):
-        rooms = self.initialize()
-        maze_start = random.choice(rooms[31])  # bottom row
-        maze_exit = random.choice(rooms[0])  # top row
-        maze_stack = Stack()
-        maze_stack.push(maze_start)
-        # repeat until stack is empty
-        while len(maze_stack):
-            room = maze_stack.get_head()
-            # pick a random, available direction
-            available_rooms = room.get_available_rooms()
-            if len(available_rooms):
-                next_room = random.choice(available_rooms)
-                # connect and add next room to stack
-                room.connect(next_room)
-                maze_stack.push(next_room)
-            # if dead end, go back through stack
-            else:
-                maze_stack.pop()
+        def get_room(self, x, y):
+            if not self.rooms:
+                self.initialize()
+            try:
+                return self.rooms[y][x]
+            except IndexError:
+                return None
 
+        def get_room_by_id(self, room_id):
+            x, y = room_id
+            return self.get_room(x, y)
 
-class Room(models.Model):
-    x = models.IntegerField(default=0)
-    y = models.IntegerField(default=0)
-    north_connection = models.BooleanField(default=False)
-    east_connection = models.BooleanField(default=False)
-    south_connection = models.BooleanField(default=False)
-    west_connection = models.BooleanField(default=False)
-    maze = models.IntegerField(default=0)
+class Room():
+    north_connection = 0
+    east_connection = 0
+    south_connection = 0
+    west_connection = 0
+
+    def __init__(self, x, y, maze, **kwargs):
+        self.id = bytes((x, y))
+        self.x = x
+        self.y = y
+        self.maze = maze
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
 
     def connect(self, room):
-        if self.y < room.y:
-            self.north_connection = True
-            room.south_connection = True
-        elif self.y > room.y:
-            self.south_connection = True
-            room.north_connection = True
+        if self.y > room.y:
+            self.north_connection = 1
+            room.south_connection = 1
+        elif self.y < room.y:
+            self.south_connection = 1
+            room.north_connection = 1
         elif self.x < room.x:
-            self.east_connection = True
-            room.west_connection = True
+            self.east_connection = 1
+            room.west_connection = 1
         elif self.x > room.x:
-            self.west_connection = True
-            room.east_connection = True
-        self.save()
-        room.save()
+            self.west_connection = 1
+            room.east_connection = 1
 
     def count_connections(self):
         count = 0
@@ -94,46 +126,41 @@ class Room(models.Model):
             if room:
                 return room.count_connections() == 0
             return False
+        
         return [*filter(is_available, rooms)]
 
     def get_room_north(self):
-        try:
-            return Room.objects.get(maze=self.maze, x=self.x, y=self.y+1)
-        except Room.DoesNotExist:
-            return None
+        return self.maze.get_room(self.x, self.y-1)
 
-    def get_room_east(self):
-        try:
-            return Room.objects.get(maze=self.maze, x=self.x+1, y=self.y)
-        except Room.DoesNotExist:
-            return None
+    def get_room_east(self, rooms):
+        return self.maze.get_room(self.x+1, self.y)
 
-    def get_room_south(self):
-        try:
-            return Room.objects.get(maze=self.maze, x=self.x, y=self.y-1)
-        except Room.DoesNotExist:
-            return None
+    def get_room_south(self, rooms):
+        return self.maze.get_room(self.x, self.y+1)
 
-    def get_room_west(self):
-        try:
-            return Room.objects.get(maze=self.maze, x=self.x-1, y=self.y)
-        except Room.DoesNotExist:
-            return None
+    def get_room_west(self, rooms):
+        return self.maze.get_room(self.x-1, self.y)
 
 
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    current_room = models.IntegerField(default=0)
+    current_maze = models.IntegerField(default=0)
+    current_room = models.BinaryField(default=bytes((0,0)))
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
 
     def initialize(self):
-        self.current_room = Room.objects.first().id
+        first_maze = Maze.objects.first()
+        self.current_maze = first_maze.id
+        self.current_room = first_maze.start_room
         self.save()
 
     def room(self):
-        try:
-            return Room.objects.get(id=self.current_room)
-        except Room.DoesNotExist:
+        maze = Maze.objects.get(id=self.current_maze)
+        if maze:
+            room = maze.get_room(self.current_room)
+        if room:
+            return room
+        else:
             self.initialize()
             return self.room()
 
