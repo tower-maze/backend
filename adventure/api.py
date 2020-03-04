2729c7a03db38059e755534a1e6005d628e55f4d
William Sedlacek <wsedlacekc@gmail.com>
2020-03-04 01:42:07 -0600

    Cleanup
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- adventure/api.py
+++ adventure/api.py
@@ -1,18 +1,13 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 # from pusher import Pusher
 from django.http import JsonResponse
 from decouple import config
-from django.contrib.auth.models import User
 from .models import *
 from rest_framework.decorators import api_view
-import json
 
 # instantiate pusher
 # pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config('PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
 
 
-@csrf_exempt
 @api_view(['GET'])
 def initialize(request):
     user = request.user
@@ -24,7 +19,6 @@ def initialize(request):
     return JsonResponse({'x': room.x, 'y': room.y, 'maze': room.maze}, safe=True)
 
 
-# @csrf_exempt
 @api_view(['POST'])
 def move(request):
     data = request.data
@@ -37,7 +31,6 @@ def move(request):
         return JsonResponse({'message': 'Invalid Direction'}, status=400)
 
 
-@csrf_exempt
 @api_view(['POST'])
 def say(request):
     # IMPLEMENT
