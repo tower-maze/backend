23ff48460a30aea992f20e864973ca6157103b62
William Sedlacek <wsedlacekc@gmail.com>
2020-03-03 23:25:19 -0600

    CORS requires https, CSRF requires no https
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
 CRSF_COOKIE_SAMESITE = None
 SESSION_COOKIE_SAMESITE = None
 
-CORS_ORIGIN_WHITELIST = ['tower-maze.netlify.com']
+CORS_ORIGIN_WHITELIST = ['https://tower-maze.netlify.com']
 CSRF_TRUSTED_ORIGINS = ['tower-maze.netlify.com']
 
 CORS_ALLOW_CREDENTIALS = True
